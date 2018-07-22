535e6cafa83a2ad42ee1617e01c41a21049b6efc
Swati <swati@c4b301d5d613.ant.amazon.com>
2018-07-22 21:30:39 +0530

    dividing app into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MaterialModule } from './material';
 import { AppRoutingModule } from './app-routing.module';
+import { BookingComponent } from './booking/booking.component';
+import { ProfileComponent } from './profile/profile.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingComponent,
+    ProfileComponent,
+    InventoryComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
